1ab0f34197bd8a64c724c2272b0c1bb878ecf2e5
Danil <danil.lyalin@gmail.com>
2022-10-06 18:46:34 +0300

    Lab 3 initial commit

---
+++ lab3/client/src/main/java/com/client/app/ClientController.java
@@ -0,0 +1,38 @@
+package com.client.app;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value="/gateway")
+public class ClientController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${endpoint.server}")
+    private String msEndpoint;
+
+    @RequestMapping(value = "/data", method = RequestMethod.GET)
+    public String getData() {
+        System.out.println("Returning data from gateway");
+        return "Hello from gateway";
+    }
+
+    @RequestMapping(value = "/server-data", method = RequestMethod.GET)
+    public String getMsData() {
+        System.out.println("Returning data from server through gateway");
+        try {
+            System.out.println("Endpoint name : [" + msEndpoint + "]");
+            return restTemplate.getForObject(new URI(msEndpoint), String.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "Exception occurred";
+    }
+}
